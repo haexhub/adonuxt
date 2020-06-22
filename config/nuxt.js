62ce18f43e5387632708066142fcf95b6407032e
Martin Drechsel (G432307) <martin.drechsel@gisa.de>
2020-06-22 09:01:54 +0200

    init commit

---
+++ config/nuxt.js
@@ -0,0 +1,127 @@
+const colors = require('vuetify/es5/util/colors').default
+const resolve = require('path').resolve
+
+module.exports = {
+  mode: 'universal',
+  srcDir: resolve(__dirname, '..', 'resources'),
+  telemetry: false,
+  /*
+  ** Headers of the page
+  */
+  head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    '@nuxt/typescript-build',
+    '@nuxtjs/vuetify',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    [ '@nuxtjs/dotenv', { path: './' } ]
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+
+  
+
+
+  router: {
+    //middleware: ['auth']
+  },
+/*
+
+
+
+scope: 'openid fhirUser patient/*.read'
+    , redirectUri: '/api/patient'
+    , clientId: 'g432307'
+    , launch: 'ausiw'
+    , clientSecret: process.env.FHIR_SECRET
+    //, fhirServiceUrl: process.env.FHIR_SERVER
+    , iss: process.env.FHIR_SERVER
+
+
+  /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {    }
+
+  , babel: {
+      presets({ isServer }) {
+        return [
+          [
+            require.resolve('@nuxt/babel-preset-app'),
+            // require.resolve('@nuxt/babel-preset-app-edge'), // For nuxt-edge users
+            {
+              buildTarget: isServer ? 'server' : 'client',
+              corejs: { version: 3 }
+            }
+          ]
+        ]
+      }
+    }
+  }
+}
---
+++ resources/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>läuft</h1>
+</template>
